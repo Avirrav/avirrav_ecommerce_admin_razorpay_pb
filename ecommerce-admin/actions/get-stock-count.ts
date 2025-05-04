e6f66f862f1f2d408f0af14fd28070690483289a
Gourav Rajak <gouravrajak+github@gmail.com>
2025-05-05 02:55:35 +0530

    Refactor DashboardPage to use hooks for state management; update stock count retrieval and add real-time clock display.

--- ecommerce-admin/actions/get-stock-count.ts
+++ ecommerce-admin/actions/get-stock-count.ts
@@ -1,11 +1,15 @@
 import prismadb from '@/lib/prismadb';
 
 export const getStockCount = async (storeId: string) => {
-  const stockCount = await prismadb.product.count({
+  const stockCount = await prismadb.product.aggregate({
     where: {
       storeId: storeId,
       isArchived: false,
     },
+    _sum: {
+      stockQuantity: true
+    }
   });
-  return stockCount;
+
+  return stockCount._sum.stockQuantity || 0;
 };
